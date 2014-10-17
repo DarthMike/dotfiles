b79bc069ead596a0b9b4b8653106f7d3c11e36a5
Miguel Angel Quinones <miguel.quinones@corp.badoo.com>
2014-10-17 13:10:34 +0100

    Changes preferred ruby version to 2.1.1

--- bashsetup.sh
+++ bashsetup.sh
@@ -6,9 +6,9 @@ curl -sSL https://get.rvm.io | bash -s stable
 echo '...Installing homebrew...'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo '...Setting up ruby 2.1.0...'
-rvm install ruby 2.1.0
-rvm use ruby-2.1.0
+echo '...Setting up ruby 2.1.1...'
+rvm install ruby 2.1.1
+rvm use ruby-2.1.1
 
 echo '...Installing ruby essentials...'
 gem install cocoapods
