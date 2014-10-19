c75984734d4bfb913f131fdc8adc7f582d8b98d4
Miguel Angel Quinones <miguel.quinones@corp.badoo.com>
2014-10-19 21:01:44 +0100

    Adds oh-my-zsh config

--- zshsetup.sh
+++ zshsetup.sh
@@ -7,3 +7,9 @@ echo "...Adding hombrew zsh to /etc/shells "
 NEWSHELL="/usr/local/bin/zsh"
 SHELLS="/etc/sheels"
 grep -q -F $NEWSHELL $SHELLS || echo $NEWSHELL >> $SHELLS
+
+echo "...Changing shell to zsh"
+chsh -s $NEWSHELL
+
+echo "...Installing oh-my-zsh..."
+curl -L http://install.ohmyz.sh | sh
