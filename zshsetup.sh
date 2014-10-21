c9db39479e8cf0d7218970d9a6ffc9f5d660a46c
Miquel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2014-10-21 10:19:05 +0100

    Changes

--- zshsetup.sh
+++ zshsetup.sh
@@ -5,11 +5,10 @@ brew install zsh
 
 echo "...Adding hombrew zsh to /etc/shells "
 NEWSHELL="/usr/local/bin/zsh"
-SHELLS="/etc/sheels"
+SHELLS="/etc/shells"
 grep -q -F $NEWSHELL $SHELLS || echo $NEWSHELL >> $SHELLS
-
-echo "...Changing shell to zsh"
-chsh -s $NEWSHELL
 
 echo "...Installing oh-my-zsh..."
 curl -L http://install.ohmyz.sh | sh
+
+echo "...Done. Change shells with: 'sudo chsh -s /usr/local/bin/zsh'"
