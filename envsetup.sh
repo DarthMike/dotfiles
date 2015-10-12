0789ced6b47fcaae88ec3775c7e852772f3f4019
Miguel Angel Quinones <m.quinones.garcia@gmail.com>
2015-10-12 15:49:32 +0100

    Parallel gem installing. Awesome stuff from ThoughtBot

--- envsetup.sh
+++ envsetup.sh
@@ -4,6 +4,10 @@ echo '...Installing ruby essentials...'
 gem install bundle
 bundle install
 
+# https://github.com/thoughtbot/laptop
+number_of_cores=`sysctl -n hw.ncpu`
+bundle config --global jobs `expr $number_of_cores - 1`
+
 echo '...Installing homebrew essentials...'
 ./homebrew.sh
 
