928f8962db45bd15089bf3773fdfa9b429841869
Miguel Angel Quinones <m.quinones.garcia@gmail.com>
2015-11-24 12:01:00 +0000

    Fix kicker script not using quoted strings for paths

--- .kick/code_injection.rb
+++ .kick/code_injection.rb
@@ -3,12 +3,19 @@ recipe :code_injection do
   process do |potential_files|
     files = potential_files.take_and_map do |file|
         if file =~ %r{^.*\.(swift|m|xib|storyboard|strings|png|jpeg|jpg)$}
-          execute("/usr/bin/python #{File.expand_path("~/.dyci/scripts/dyci-recompile.py")} #{File.expand_path(file)}")
+          cmd = ['/usr/bin/python']
+          cmd << File.expand_path('~/.dyci/scripts/dyci-recompile.py').safe_shell_path
+          cmd << File.expand_path(file).safe_shell_path
+
+          command = cmd.join(' ')
+          puts "Executing #{command}"
+          execute(command)
           puts "DYCI: Recompiled #{file}"
           file
         end
       end
   end
+
 
   process do |remaining_files|
     remaining_files.take_and_map do |file|
@@ -21,4 +28,10 @@ recipe :code_injection do
     log "DYCI: Kicker watching for file changes to recompile!"
   end
 
+end
+
+class String
+  def safe_shell_path
+    "\"#{self}\""
+  end
 end
