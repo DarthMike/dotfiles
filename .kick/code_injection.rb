154d9da9396ef8087010821388f3a551336b1f51
Miguel Angel Quinones <m.quinones.garcia@gmail.com>
2015-12-03 16:07:03 +0000

    Add plist, json, html, css, js files to injectable bundle files

--- .kick/code_injection.rb
+++ .kick/code_injection.rb
@@ -2,7 +2,7 @@ recipe :code_injection do
 
   process do |potential_files|
     files = potential_files.take_and_map do |file|
-        if file =~ %r{^.*\.(swift|m|xib|storyboard|strings|png|jpeg|jpg)$}
+        if file =~ %r{^.*\.(swift|m|xib|storyboard|strings|png|jpeg|jpg|plist|json|html|css|js)$}
           cmd = ['/usr/bin/python']
           cmd << File.expand_path('~/.dyci/scripts/dyci-recompile.py').safe_shell_path
           cmd << File.expand_path(file).safe_shell_path
