c2b6572a0fed4713525473de9e4afb3d2608e866
Miguel Angel Quinones <miguel@miqu.me>
2016-04-13 18:54:49 +0100

    add git standup

---
+++ gitsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "...Installing git-standup (https://github.com/kamranahmedse/git-standup)..."
+
+curl -L https://raw.githubusercontent.com/kamranahmedse/git-standup/master/installer.sh | sudo sh
