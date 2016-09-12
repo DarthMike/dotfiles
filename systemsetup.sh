450429369b05cad45c57ec0a739f533864df2e0c
Miguel Angel Quinones <miguel@miqu.me>
2016-09-12 13:21:59 +0100

    [CHANGE] System ruby version -> 2.2.2

--- systemsetup.sh
+++ systemsetup.sh
@@ -7,9 +7,9 @@ echo '...Installing homebrew...'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/boneyard
 
-echo '...Setting up ruby 2.1.2...'
-rvm install ruby 2.1.2
-rvm alias create default ruby-2.1.2
+echo '...Setting up ruby 2.2.2...'
+rvm install ruby 2.2.2
+rvm alias create default ruby-2.2.2
 rvm reload
 
 echo '...Installing wireshark...'
